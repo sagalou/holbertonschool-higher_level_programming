3f176f631ac7326742fe3312ef4ef7606dc4c301
sagalou <sagal.louise.haider@gmail.com>
2026-05-26 15:06:50 +0200

    add task_01_pickle.py file

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Module that serialize and deserialize custom Python objects"""
+
+import pickle
+
+
+class CustomObject:
+
+    """Class that represents custom objects"""
+
+    def __init__(self, name, age, is_student):
+        """Iniatialize a neww custom objects"""
+
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """Display the atributs"""
+
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("Is Student:", self.is_student)
+
+    def serialize(self, filename):
+        """Prototype to serialize the specified file"""
+
+        with open(filename, "wb") as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        """Prototype to deserialize from the specified file"""
+
+        with open(filename, "rb") as f:
+            return pickle.load(f)
