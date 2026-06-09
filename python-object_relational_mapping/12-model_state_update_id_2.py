e7cd29a1cdc68f5f80321d8d16b565cfd5eaca7c
sagalou <sagal.louise.haider@gmail.com>
2026-06-09 12:43:23 +0200

    add 12-model_state_update_id_2 file

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module that changes the name of a State object"""
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
+    session = Session(engine)
+    Base.metadata.create_all(engine)
+    query_rows = session.query(State).order_by(State.id).filter(
+        State.id == 2).first()
+    print("{}: {}".format(query_rows.id, query_rows.name))
+    session.close()
