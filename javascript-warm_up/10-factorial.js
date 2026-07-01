f2d31e2a762419bfd5e2a4184dfa00a743ad38a3
sagalou <sagal.louise.haider@gmail.com>
2026-07-01 13:05:55 +0200

    add 10-factorial.js

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (n === 0 || n === 1 || isNaN(n)) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const x = parseInt(process.argv[2]);
+
+console.log(factorial(x));
